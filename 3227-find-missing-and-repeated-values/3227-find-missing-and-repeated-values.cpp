b477bd2b3f4cf42236e7d28f092c8cff9c3e5ede
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2024-06-17 23:44:37 +0530

    Time: 12 ms (93.79%), Space: 25 MB (91%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int> result(2,-1);
-        unordered_map<int, int> mp;
-        for (auto& vec : grid) {
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& a) {
+        long long n = a.size()*a[0].size();
+        long long SN = (n * (n + 1)) / 2;
+        long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+        long long S = 0, S2 = 0;
+        for (auto& vec : a) {
             for (int num : vec) {
-                mp[num]++;
+                S += num;
+                S2 += (long long)num * (long long)num;
             }
         }
+        long long val1 = S - SN;
+        long long val2 = S2 - S2N;
+        val2 = val2 / val1;
+        long long x = (val1 + val2) / 2;
+        long long y = x - val1;
 
-        int maxNum = grid.size() * grid[0].size();
-        for (int i = 1; i <= maxNum; ++i) {
-            if (mp[i] == 0) {
-                result[1] = i; 
-            } else if (mp[i] == 2) {
-                result[0] = i; 
-            }
-        }
-
-        return result;
+        return {(int)x, (int)y};
     }
 };
