6a40ab6574322bbd8c8ed266a410d05aebcc8e16
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2024-07-25 22:31:01 +0530

    Time: 627 ms (20.47%), Space: 284.2 MB (20.36%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -6,20 +6,16 @@ public:
         int right = mid + 1;
         while (left <= mid && right <= high) {
             if (arr[left] <= arr[right]) {
-                temp.push_back(arr[left]);
-                left++;
+                temp.push_back(arr[left++]);
             } else {
-                temp.push_back(arr[right]);
-                right++;
+                temp.push_back(arr[right++]);
             }
         }
         while (left <= mid) {
-            temp.push_back(arr[left]);
-            left++;
+            temp.push_back(arr[left++]);
         }
         while (right <= high) {
-            temp.push_back(arr[right]);
-            right++;
+            temp.push_back(arr[right++]);
         }
         for (int i = low; i <= high; i++) {
             arr[i] = temp[i - low];
