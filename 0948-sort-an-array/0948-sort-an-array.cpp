301eca648fa799e2bf854809b1b07271fda18c77
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2024-07-25 22:34:36 +0530

    Time: 64 ms (98.32%), Space: 78.6 MB (49.24%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    void merge(vector<int>& arr, int low, int mid, int high) {
-        vector<int> temp;
-        int left = low;
-        int right = mid + 1;
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                temp.push_back(arr[left++]);
-            } else {
-                temp.push_back(arr[right++]);
+    vector<int> sortArray(vector<int>& nums) {
+        vector<int> counting(2 * 50000 + 1, 0);
+        for (int num : nums) {
+            // we add 5 * 10^4 because for smallest possible element -5 * 10^4
+            // index must be 0
+            counting[num + 50000]++;
+        }
+        int write_ind = 0;
+        for (int number_ind = 0; number_ind < counting.size(); ++number_ind) {
+            int freq = counting[number_ind];
+            while (freq != 0) {
+                nums[write_ind] = number_ind - 50000;
+                write_ind++;
+                freq--;
             }
         }
-        while (left <= mid) {
-            temp.push_back(arr[left++]);
-        }
-        while (right <= high) {
-            temp.push_back(arr[right++]);
-        }
-        for (int i = low; i <= high; i++) {
-            arr[i] = temp[i - low];
-        }
-    }
-
-    void mergeSort(vector<int>& arr, int low, int high) {
-        if (low >= high)
-            return;
-        int mid =(low + high) / 2;
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
-        merge(arr, low, mid, high);
-    }
-
-    vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums,0,nums.size()-1);
         return nums;
     }
 };
