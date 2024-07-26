1682454105a07af5f0d97bddadb3cd1d7c3db556
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2024-07-26 22:43:58 +0530

    Time: 26 ms (61.94%), Space: 15.5 MB (79.53%) - LeetHub

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> distance(n, vector<int>(n, INT_MAX));
+        for (int i = 0; i < n; ++i) {
+            distance[i][i] = 0;
+        }
+        for (const auto& edge : edges) {
+            int node1 = edge[0], node2 = edge[1], dist = edge[2];
+            distance[node1][node2] = dist;
+            distance[node2][node1] = dist;
+        }
+        for (int midle = 0; midle < n; ++midle) {
+            for (int source = 0; source < n; ++source) {
+                for (int destination = 0; destination < n; ++destination) {
+                    if (distance[source][midle] < INT_MAX &&
+                        distance[midle][destination] < INT_MAX) {
+                        distance[source][destination] =
+                            min(distance[source][destination],
+                                distance[source][midle] +
+                                    distance[midle][destination]);
+                    }
+                }
+            }
+        }
+        int minimum_number = n;
+        int res = -1;
+        for (int source = 0; source < n; ++source) {
+            int source_count = 0;
+            for (int destination = 0; destination < n; ++destination) {
+                if (distance[source][destination] <= distanceThreshold) {
+                    ++source_count;
+                }
+            }
+            if (source_count <= minimum_number) {
+                minimum_number = source_count;
+                res = source;
+            }
+        }
+        return res;
+    }
+};
