f8223c85db986239ae5c1dbb4d56647bf3d92388
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-06-27 14:49:51 +0530

    Time: 0 ms (100%), Space: 14.1 MB (73.32%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void func(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int > ds){
+    void func(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int > &ds){
         if(ind==arr.size()){
             if(target==0){
                 ans.push_back(ds);
