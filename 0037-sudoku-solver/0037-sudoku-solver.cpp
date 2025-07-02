f9cef5fa298a7477256c84dede74fc51bc62695c
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-07-02 18:07:17 +0530

    Time: 462 ms (25.37%), Space: 8.8 MB (28.29%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -7,12 +7,12 @@ public:
             for (int j = 0; j < board[0].size(); j++) {
                 if (board[i][j] == '.') {
                     for (char c = '1'; c <= '9'; c++) {
-                        if (isvalid(board, i, j, c)) {
+                        if (isValid(board, i, j, c)) {
                             board[i][j] = c;
                             if (solve(board) == true)
                                 return true;
                             else
-                                board[i][j]='.';
+                                board[i][j] = '.';
                         }
                     }
                     return false;
@@ -22,11 +22,11 @@ public:
         return true;
     }
 
-    bool isvalid(vector<vector<char>>& board, int row, int col, char c) {
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
         for (int i = 0; i < 9; i++) {
-            if (board[i][col] == c)
-                return false;
             if (board[row][i] == c)
+                return false;
+            if (board[i][col] == c)
                 return false;
             if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
                 return false;
