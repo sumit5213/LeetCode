61236b1be0f3eb53099f7d5619c7f7cf7179ce1c
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-07-19 20:30:22 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.3%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,9 +5,7 @@ public:
         int sum = 0;
         for (int i = 0; i < arr.size(); i++) {
             sum += arr[i];
-            if (sum > maxi) {
-                maxi = sum;
-            }
+            maxi=max(maxi,sum);
             if (sum < 0) {
                 sum = 0;
             }
