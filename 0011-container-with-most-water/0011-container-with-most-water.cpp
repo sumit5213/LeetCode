866019bec0b4c72e98031937060ebdd67ff7f391
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-09-16 00:14:23 +0530

    Time: 4 ms (21.74%), Space: 63.1 MB (12.19%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        int area = 0, p1 = 0, p2 = height.size() - 1;
-        int tmpMax;
-        while (p1 < p2) {
-            if (height[p1] < height[p2]) {
-                tmpMax = height[p1] * (p2 - p1);
-                if (area < tmpMax)
-                    area = tmpMax;
-                p1++;
-            } else {
-                tmpMax = height[p2] * (p2 - p1);
-                if (area < tmpMax)
-                    area = tmpMax;
-                p2--;
+        int ans=0, tempMax = 0, i=0, j=height.size()-1;
+        while(i<j){
+            if(height[i]<height[j]){
+                tempMax = height[i]*(j-i);
+                if(ans<tempMax) ans=tempMax;
+                i++;
+            }else{
+                tempMax = height[j]*(j-i);
+                if(ans<tempMax) ans=tempMax;
+                j--;
             }
         }
-        return area;
+        return ans;
     }
 };
