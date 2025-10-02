dc4bcd7af0ed4868f85be0f89487b4a6149ddda6
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-10-02 19:07:56 +0530

    Time: 15 ms (71.19%), Space: 25 MB (65.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,13 +4,13 @@ public:
         unordered_map<string, vector<string>> mp;
         vector<vector<string>> ans;
 
-        for(auto s: strs){
+        for(auto& s: strs){
             string key=s;
             sort(key.begin(), key.end());
             mp[key].push_back(s);
         }
 
-        for(auto entry: mp){
+        for(auto& entry: mp){
             ans.push_back(entry.second);
         }
         return ans;
