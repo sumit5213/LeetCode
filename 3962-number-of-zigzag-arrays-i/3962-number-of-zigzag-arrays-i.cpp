a35d798a7772818683fdbf62d0ee77fc6966f3ba
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-10-02 14:48:59 +0530

    Time: 455 ms (87.83%), Space: 488.3 MB (44.48%) - LeetHub

---
+++ 3962-number-of-zigzag-arrays-i/3962-number-of-zigzag-arrays-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int zigZagArrays(int n, int l, int r) {
+        int MOD = 1e9 + 7;
+        vector<int> dp(r + 1);
+        fill(dp.begin(), dp.end(), 1);
+
+        for (int i = 1; i < n; i++) {
+            vector<int> next_dp(r + 1);
+            if (i % 2 == 1) {
+                int prefix = 0;
+                for (int x = l; x <= r; x++) {
+                    next_dp[x] = prefix;
+                    prefix = (prefix + dp[x]) % MOD;
+                }
+            } else {
+                int suffix = 0;
+                for (int x = r; x >= l; x--) {
+                    next_dp[x] = suffix;
+                    suffix = (suffix + dp[x]) % MOD;
+                }
+            }
+            dp = next_dp;
+        }
+
+        int count = 0;
+        for (int i = l; i <= r; i++) {
+            count = (count + dp[i]) % MOD;
+        }
+        return (count * 2) % MOD;
+    }
+};
