e067e5e3283904d2d38a60d3e1589f70baa78880
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-10-02 16:13:07 +0530

    Time: 446 ms (88.27%), Space: 488.1 MB (45.13%) - LeetHub

--- 3962-number-of-zigzag-arrays-i/3962-number-of-zigzag-arrays-i.cpp
+++ 3962-number-of-zigzag-arrays-i/3962-number-of-zigzag-arrays-i.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int zigZagArrays(int n, int l, int r) {
-        int MOD = 1e9 + 7;
-        vector<int> dp(r + 1, 1);
-        
-        for (int i = 1; i < n; i++) {
-            vector<int> next_dp(r + 1);
-            if (i % 2 == 1) {
-                int prefix = 0;
-                for (int x = l; x <= r; x++) {
-                    next_dp[x] = prefix;
-                    prefix = (prefix + dp[x]) % MOD;
+        int mod = 1e9+7;
+        vector<int> dp(r+1, 1);
+        for(int i=1;i<n;i++){
+            vector<int> next_dp(r+1);
+            if(i%2==1){
+                int prefix=0;
+                for(int x=l;x<=r;x++){
+                    next_dp[x]=prefix;
+                    prefix=(prefix+dp[x])%mod;
                 }
-            } else {
+            }else{
                 int suffix = 0;
-                for (int x = r; x >= l; x--) {
-                    next_dp[x] = suffix;
-                    suffix = (suffix + dp[x]) % MOD;
+                for(int x=r;x>=l;x--){
+                    next_dp[x]=suffix;
+                    suffix=(suffix+dp[x])%mod;
                 }
             }
-            dp = next_dp;
+            dp=next_dp;
         }
-
-        int count = 0;
-        for (int i = l; i <= r; i++) {
-            count = (count + dp[i]) % MOD;
+        int count =0;
+        for(int i=l;i<=r;i++){
+            count = (count+dp[i])%mod;
         }
-        return (count * 2) % MOD;
+        return (count*2)%mod;
     }
 };
