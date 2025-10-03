91476270091ae777abeac54cd9399624a22c0643
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-10-03 21:51:09 +0530

    Time: 0 ms (100%), Space: 27.3 MB (30.86%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>prefixMax(n);
-        vector<int>suffixMax(n);
+        int n = nums.size();
+        vector<int> prefixMax(n);
+        vector<int> suffixMax(n);
 
-        prefixMax[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefixMax[i]=max(prefixMax[i-1], nums[i]);
+        prefixMax[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = max(prefixMax[i - 1], nums[i]);
         }
 
-        suffixMax[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffixMax[i] = max(suffixMax[i+1], nums[i]);
+        suffixMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMax[i] = max(suffixMax[i + 1], nums[i]);
         }
 
-        int total=0;
-
-        for(int i=0;i<n;i++){
-            int leftMax=prefixMax[i];
-            int rightMax=suffixMax[i];
-            if(nums[i]<leftMax && nums[i]<rightMax){
-                total+=min(leftMax, rightMax)-nums[i];
-            }
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += min(prefixMax[i], suffixMax[i]) - nums[i];
         }
         return total;
     }
