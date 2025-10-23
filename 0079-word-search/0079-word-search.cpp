ae854b5a117cc682c7f1006893a1f7fee333aebf
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-10-23 15:23:38 +0530

    Time: 780 ms (32.88%), Space: 11.7 MB (33.64%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    bool search(vector<vector<char>>& board, string word, int row, int col,
-                int initial, int m, int n) {
-        if (initial == word.length())
-            return true;
-        if (row < 0 or col < 0 or row == m or col == n or
-            board[row][col] != word[initial] or board[row][col] == '!')
-            return false;
-
-        char c = board[row][col];
-        board[row][col] = '!';
-
-        bool top = search(board, word, row - 1, col, initial + 1, m, n);
-        bool right = search(board, word, row, col + 1, initial + 1, m, n);
-        bool bottom = search(board, word, row + 1, col, initial + 1, m, n);
-        bool left = search(board, word, row, col - 1, initial + 1, m, n);
-
-        board[row][col] = c;
-
-        return top || right || bottom || left;
-    }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
+        int m=board.size();
+        int n=board[0].size();
 
-        int index = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (board[i][j] == word[index]) {
-                    if (search(board, word, i, j, index, m, n))
+        int index=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[index]){
+                    if(search(board, word, i, j, index, m, n)){
                         return true;
+                    }
                 }
             }
         }
-
         return false;
+    }
+
+    bool search(vector<vector<char>>& board, string word, int row, int col, int initial, int m, int n){
+        if(initial==word.length()) return true;
+        if(row<0 or row==m or col<0 or col==n or board[row][col]!=word[initial] or board[row][col] == '!') 
+            return false;
+        int roww[4] = {-1, 0, 1, 0};
+        int coll[4] = {0, 1, 0, -1};
+        char c = board[row][col];
+        board[row][col] = '!';
+        bool found=false;
+        for(int i=0;i<4;i++){
+            int newRow = row + roww[i];
+            int newCol = col + coll[i];
+                if(search(board, word, newRow, newCol, initial+1, m, n)){
+                    found=true;
+                    break;
+                }
+        }
+        board[row][col] = c;
+        return found;
     }
 };
