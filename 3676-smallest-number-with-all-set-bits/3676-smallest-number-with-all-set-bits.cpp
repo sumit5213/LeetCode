7d0fe93dfb9ee056502eb6b8f47de7ac9b6d899a
Sumit Joshi <95459098+sumit5213@users.noreply.github.com>
2025-10-29 16:27:45 +0530

    Time: 0 ms (100%), Space: 8.3 MB (60.8%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        vector<int> temp = {1, 3, 7, 15, 31, 63, 127, 255, 511, 1023};
-        for (int i = 0; i < temp.size(); i++) {
-            if (n == temp[i])
-                return n;
-            if (n < temp[i])
-                return temp[i];
+        int ans=1;
+        while(ans<n){
+            ans=ans*2+1;
         }
-        return 1;
+        return ans;
     }
 };
